e1b72e5df2a5ec11dfd2d1e1458f2bb43a92ca33
DERRICK GONG (DSCOE-DSDM-ISD-OOCLL/SHA) <GONGDE@OOCL.COM>
2025-08-21 17:38:56 +0800

    inline variables

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,29 +9,26 @@ public class WordFrequencyGame {
         } else {
 
             try {
-
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
 
-                List<Input> inputList = Arrays.stream(arr)
-                        .map(s -> new Input(s, 1))
-                        .collect(Collectors.toList());
+                List<Input> inputList;
 
                 //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map = getListMap(inputList);
 
-                inputList = map.entrySet().stream()
+                inputList = getListMap(Arrays.stream(inputStr.split("\\s+"))
+                        .map(s -> new Input(s, 1))
+                        .collect(Collectors.toList())).entrySet().stream()
                         .map(entry -> new Input(entry.getKey(), entry.getValue().size()))
                         .collect(Collectors.toList());
 
                 inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
-                StringJoiner joiner = new StringJoiner("\n");
                 inputList.stream()
                         .map(w -> w.getValue() + " " + w.getWordCount())
-                        .forEach(joiner::add);
+                        .forEach(new StringJoiner("\n")::add);
 
-                return joiner.toString();
+                return new StringJoiner("\n").toString();
+
             } catch (Exception e) {
 
                 return "Calculate Error";
